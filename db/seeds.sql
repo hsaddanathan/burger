3718625c21a2437465a40316bcebaaf8c8e91b0b
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-12-05 21:23:05 -0500

    Added npm packages. ORM,Config, schema, server.js

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ("House Burger");
+INSERT INTO burgers (name) VALUES ("House Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Barbecue Bacon Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Cowboy Burger");
+INSERT INTO burgers (name) VALUES ("Impossible Burger");
+
+select * from burgers
